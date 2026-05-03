7da74a61cc9f3b4247a3eca55dba954d8220ea34
Álvaro <alvex.c.c.flow@gmail.com>
2026-05-03 12:16:51 -0300

    Fix div function logic and improve output

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def som(a: int, b: int): # Pega os valores
 @app.command()
 def sub(a: int, b: int): # Pega os valores
     print("Subtração: ", a - b)  #Subtrai dois números.
+
 @app.command()
 def mul(a: int, b: int): # Pega os valores
     resultado = 0
@@ -16,18 +17,22 @@ def mul(a: int, b: int): # Pega os valores
         print(resultado, "+", a, end=" ") # Mostra a operação feita
         resultado += a # Faz a operação
         print("=", resultado) # Mostra o resultado final
+
 @app.command()
 def div(a: int, b: int): # Pega os valores
     resultado = 0 
-    for _ in range(a):
+    for x in range(a):
         if a < b: # Para quando não dá mais para subtrair b de a
             break
+
         print(a, "-", b, "=", a - b) # Mostra a operação feita
         a -= b # Faz a operação
-        resultado +=  1 # Grava quantas vezes se repetiu
+        resultado +=  1 # Grava quantas vezes se repetiu 
     
     print("Resultado: Repetiu", resultado, "Vezes") # Mostra quantas vezes se repetiu
-    print("Resto:") # Mostra o resto
+    print("Resto:", a) # Mostra o resto
         
+    
+    
 if __name__ == "__main__":
     app() 
